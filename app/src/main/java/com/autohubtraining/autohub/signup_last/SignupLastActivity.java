8f19d1658567c687999649f643d5ddcf0e5d8885
Tushar <sarkartushar1206@gmail.com>
2019-05-28 20:52:04 +0530

    feat: Initialize firebase & Signup last UI (WIP)
    # Initialized Firebase in the project.
    # UI development in progress.

---
+++ app/src/main/java/com/autohubtraining/autohub/signup_last/SignupLastActivity.java
@@ -0,0 +1,19 @@
+package com.autohubtraining.autohub.signup_last;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.autohubtraining.autohub.R;
+
+public class SignupLastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_last);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
