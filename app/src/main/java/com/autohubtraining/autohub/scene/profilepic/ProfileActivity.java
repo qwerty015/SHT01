69f2370548fa7f8d79600118a2a5cf456a4f0297
Tushar <sarkartushar1206@gmail.com>
2019-05-30 15:51:07 +0530

    feat: image picker added

--- app/src/main/java/com/autohubtraining/autohub/scene/profilepic/ProfileActivity.java
+++ app/src/main/java/com/autohubtraining/autohub/scene/profilepic/ProfileActivity.java
@@ -1,21 +1,27 @@
 package com.autohubtraining.autohub.scene.profilepic;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.autohubtraining.autohub.R;
 import com.autohubtraining.autohub.scene.BaseActivity;
 import com.autohubtraining.autohub.scene.camerainfo.CameraInfoActivity;
+import com.autohubtraining.autohub.util.ImageUtils;
 
-
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends BaseActivity implements ProfileContract.View {
-
+    @BindView(R.id.profilePic)
+    CircleImageView profilePic;
     private ProfilePresenter presenter;
+    private ImageUtils imageUtils;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +33,21 @@ public class ProfileActivity extends BaseActivity implements ProfileContract.Vie
     }
 
     private void setup() {
+        imageUtils = new ImageUtils(this, null);
         presenter = new ProfilePresenter(this);
     }
 
 
-    @OnClick({R.id.nextBtn})
+    @OnClick({R.id.nextBtn, R.id.profilePic, R.id.icAdd})
     void onClickItems(View view) {
         int id = view.getId();
         switch (id) {
             case R.id.nextBtn:
                 presenter.onNextBtnClicked();
+                break;
+            case R.id.profilePic:
+            case R.id.icAdd:
+                imageUtils.chooseFromLibrary();
                 break;
         }
     }
@@ -44,5 +55,22 @@ public class ProfileActivity extends BaseActivity implements ProfileContract.Vie
     @Override
     public void navigateToNextScreen() {
         Intent intent = new Intent(this, CameraInfoActivity.class);
-        startActivity(intent);    }
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == ImageUtils.REQUEST_LOAD_IMAGE && null != data) {
+                imageUtils.onActivityResult(requestCode, resultCode, data);
+                onImageTakenFromGallery();
+            }
+        }
+    }
+
+    private void onImageTakenFromGallery() {
+        Bitmap bitmap = ImageUtils.getCompressedScaledBitmap(imageUtils.getBitmap());
+        profilePic.setImageBitmap(bitmap);
+    }
 }
