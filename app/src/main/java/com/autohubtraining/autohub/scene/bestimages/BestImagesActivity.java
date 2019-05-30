996c717f8eb33dae8cf81b24273e6b0d908c1c3b
Tushar <sarkartushar1206@gmail.com>
2019-05-30 11:18:08 +0530

    feat: best image design

--- app/src/main/java/com/autohubtraining/autohub/scene/bestimages/BestImagesActivity.java
+++ app/src/main/java/com/autohubtraining/autohub/scene/bestimages/BestImagesActivity.java
@@ -2,18 +2,27 @@ package com.autohubtraining.autohub.scene.bestimages;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.autohubtraining.autohub.R;
 import com.autohubtraining.autohub.scene.BaseActivity;
 import com.autohubtraining.autohub.scene.finalscreen.LastActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class BestImagesActivity extends BaseActivity implements BestImagesContract.View {
 
+    @BindView(R.id.image1)
+    ImageView image1;
+    @BindView(R.id.image2)
+    ImageView image2;
+    @BindView(R.id.image3)
+    ImageView image3;
+    @BindView(R.id.image4)
+    ImageView image4;
     private BestImagesPresenter presenter;
 
     @Override
@@ -30,12 +39,20 @@ public class BestImagesActivity extends BaseActivity implements BestImagesContra
     }
 
 
-    @OnClick({R.id.nextBtn})
+    @OnClick({R.id.nextBtn, R.id.image1, R.id.image2, R.id.image3, R.id.image4})
     void onClickItems(View view) {
         int id = view.getId();
         switch (id) {
             case R.id.nextBtn:
                 presenter.onNextBtnClicked();
+                break;
+            case R.id.image1:
+                break;
+            case R.id.image2:
+                break;
+            case R.id.image3:
+                break;
+            case R.id.image4:
                 break;
         }
     }
