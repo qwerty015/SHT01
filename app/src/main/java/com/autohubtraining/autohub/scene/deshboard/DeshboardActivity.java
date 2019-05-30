5d307e455a6bc3f122028a6ffdf919dfa495363b
Tushar <sarkartushar1206@gmail.com>
2019-05-30 17:55:37 +0530

    WIP:1. Design improvement screen 8,11 2. worked on design of user profile.

---
+++ app/src/main/java/com/autohubtraining/autohub/scene/deshboard/DeshboardActivity.java
@@ -0,0 +1,15 @@
+package com.autohubtraining.autohub.scene.deshboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.autohubtraining.autohub.R;
+
+public class DeshboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
